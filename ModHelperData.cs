4682316ebd89678469d3ef00b2c7c0210eb25963
Commander-Cat101 <102434202+Commander-Cat101@users.noreply.github.com>
2022-08-09 07:16:12 +1200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,7 +4,7 @@ public static class ModHelperData
 {
     public const string Name = "Snore Monkey";
     public const string Description = "After a long day of fighting the boomerang monkey fell asleep on the battlefield. 15 Upgrades varying from map wide range to a insane snoring machine gun. Comes with Ultimate Crosspathing support.";
-    public const string Version = "2.0.0";
+    public const string Version = "2.1.0";
     public const string RepoOwner = "Commander-Cat101";
     public const string RepoName = "SnoreMonkey";
     public const string Author = "Commander__Cat";
